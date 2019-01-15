4c8c18018b364fefcf87ba817b44e7736fdb8abb
wb-zh388722 <wb-zh388722@alibaba-inc.com>
2019-01-15 23:42:58 +0800

    当一个接口有多种实现时，可以用group区分

---
+++ com-jdk/src/main/java/com/tujia/comjdk/collection/MyCollections.java
@@ -0,0 +1,19 @@
+package com.tujia.comjdk.collection;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * 描述:
+ *
+ * @author zhouheng
+ * @create 2019-01-15 下午 10:42
+ */
+public class MyCollections {
+    public static void main(String[] args) {
+        CopyOnWriteArrayList copyOnWriteArrayList = new CopyOnWriteArrayList();
+        for (int i=0;i<10;i++){
+            copyOnWriteArrayList.add(i);
+        }
+        System.out.println(copyOnWriteArrayList.size());
+    }
+}
