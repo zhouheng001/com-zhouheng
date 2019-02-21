17ed890cf82bbc6dd549148be1db1aa69375c9fa
wb-zh388722 <wb-zh388722@alibaba-inc.com>
2019-02-21 20:31:20 +0800

    spring源码解析

--- com-mianshi/src/main/java/suanfa/study/Test.java
+++ com-mianshi/src/main/java/suanfa/study/Test.java
@@ -15,13 +15,13 @@ public class Test {
 
         // feibonaqie 斐波那切数列
         //1、实现
-        int i = xunHuan(6);
-        System.out.println(i);
+//        int i = xunHuan(6);
+//        System.out.println(i);
         //递归
         int i1 = diGui(6);
         System.out.println(i1);
-        //
-        c(10);
+//
+//        c(10);
         //递归文件
 //        diGuiFile(new File("F:\\英雄时刻"));
 
@@ -55,28 +55,27 @@ public class Test {
     static int diGui(int num) {
         if (num == 1 || num == 2) {
             return 1;
-        } else {
-            return diGui(num-1)+diGui(num-2);
         }
+        return diGui(num - 1) + diGui(num - 2);
     }
 
-    static void diGuiFile(File file){
+    static void diGuiFile(File file) {
 
         File[] files = file.listFiles();
         for (File file1 : files) {
-            if(file1.isFile()){
+            if (file1.isFile()) {
                 System.out.println(file1.getName());
-            }else {
+            } else {
                 diGuiFile(file1);
             }
         }
 
     }
 
-    static void  c(int a){
-        if(a==5){
+    static void c(int a) {
+        if (a == 5) {
             System.out.println(a);
-        }else {
+        } else {
             a--;
             c(a);
         }
