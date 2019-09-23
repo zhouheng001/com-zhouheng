c892b58f406a9892400f32853f61c2c996f1beb5
zhouheng-os <zhouheng-os@360os.com>
2019-09-23 16:02:54 +0800

    自定义DTD约束,倒数数组输出练手

--- com-dubbo-provider/src/main/java/com/tujia/com/dubbo/provider/ComDubboProviderApplication.java
+++ com-dubbo-provider/src/main/java/com/tujia/com/dubbo/provider/ComDubboProviderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
 
 @SpringBootApplication
 @EnableTransactionManagement
