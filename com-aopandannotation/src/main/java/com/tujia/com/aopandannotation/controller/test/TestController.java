9bd7f6691787a4825ff55360482079ecb5a56663
1020886351@qq.com <zhouheng-os@360os.com>
2019-10-28 22:45:07 +0800

    增加swagger配置

--- com-aopandannotation/src/main/java/com/tujia/com/aopandannotation/controller/test/TestController.java
+++ com-aopandannotation/src/main/java/com/tujia/com/aopandannotation/controller/test/TestController.java
@@ -8,10 +8,19 @@ import com.tujia.com.aopandannotation.service.TestService;
 import com.tujia.comdubboapi.annotation.ApiAspectLog;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 描述:
@@ -24,6 +33,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private static final String[] FILE_HEADER_MAPPING = {"id", "APP","NAME"};
+
     @Autowired
     private TestService testService;
 
@@ -34,8 +45,31 @@ public class TestController {
     @RequestMapping("/")
     @TestAnnotation(testEnum = TestEnum.PC, name = "获取数量")
     @ApiAspectLog
-    public int getCount(@RequestParam(value = "a",defaultValue = "3") int a) {
+    public int getCount(@RequestParam(value = "a", defaultValue = "3") int a) {
         return testService.getCount() + testPresentation.getSeconds();
     }
 
+    @ApiOperation(value = "导入", httpMethod = "GET", notes = "获取数量")
+    @RequestMapping(value = "/importFile",method = RequestMethod.POST)
+    @TestAnnotation(testEnum = TestEnum.PC, name = "获取数量")
+    @ApiAspectLog
+    public String importFile(@RequestParam(value = "file") MultipartFile file) throws IOException {
+
+        String name = file.getOriginalFilename();
+        System.out.println(name);
+        List<String> dataList=new ArrayList<String>();
+
+        BufferedReader br=null;
+
+        InputStream inputStream = file.getInputStream();
+        br = new BufferedReader(new InputStreamReader(inputStream,"gbk"));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                dataList.add(line);
+            }
+
+        System.out.println(dataList);
+
+        return "success";
+    }
 }
