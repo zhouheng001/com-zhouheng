28903de7fcc0c2fa4223b86e809125a3eb5082f3
1020886351@qq.com <zhouheng-os@360os.com>
2019-11-27 00:40:22 +0800

    *******

--- com-rabbitmq/com-springboot-rabbitmq-product/src/test/java/comrabbitmq/comspringbootrabbitmqproduct/ComSpringbootRabbitmqProductApplicationTests.java
+++ com-rabbitmq/com-springboot-rabbitmq-product/src/test/java/comrabbitmq/comspringbootrabbitmqproduct/ComSpringbootRabbitmqProductApplicationTests.java
@@ -1,6 +1,6 @@
 package comrabbitmq.comspringbootrabbitmqproduct;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
