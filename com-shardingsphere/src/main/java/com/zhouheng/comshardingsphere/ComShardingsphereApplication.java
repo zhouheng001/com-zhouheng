9049a1741786587363c7a4fd80e38a9e46b49e52
zhouheng-os <zhouheng-os@360os.com>
2019-11-27 15:05:16 +0800

    增加shardingsphere模块

---
+++ com-shardingsphere/src/main/java/com/zhouheng/comshardingsphere/ComShardingsphereApplication.java
@@ -0,0 +1,13 @@
+package com.zhouheng.comshardingsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComShardingsphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComShardingsphereApplication.class, args);
+    }
+
+}
---
+++ com-shardingsphere/src/test/java/com/zhouheng/comshardingsphere/ComShardingsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhouheng.comshardingsphere;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComShardingsphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
